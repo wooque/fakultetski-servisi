623833b6901eaa0a6801a0b6045116d3dc680580
wooque <wooque@gmail.com>
2013-07-07 16:56:16 +0200

    Minor bugfix regarding adjusting of lab ending

--- src/java/com/corejsf/Lab.java
+++ src/java/com/corejsf/Lab.java
@@ -59,9 +59,17 @@ public class Lab implements Serializable{
     public void setEnd(Date end) { this.end = end; }
     
     public Date getAdjustEnd() { return end; }
-    public void setAdjustEnd(Date end) { 
-        Date temp = new Date((this.end.getTime()/(24*3600*1000))*(24*3600*1000));
-        this.end = new Date(temp.getTime() + end.getTime()); 
+    public void setAdjustEnd(Date end) {
+        Calendar cal = Calendar.getInstance();
+        cal.setTime(this.end);
+        int year = cal.get(Calendar.YEAR);
+        int month = cal.get(Calendar.MONTH);
+        int day = cal.get(Calendar.DAY_OF_MONTH);
+        cal.setTime(end);
+        int hour = cal.get(Calendar.HOUR_OF_DAY);
+        int minute = cal.get(Calendar.MINUTE);
+        cal.set(year, month, day, hour, minute);
+        this.end = cal.getTime(); 
     }
     
     public Date getTomorrow() {
